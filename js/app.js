a9d237dc950e1bf5f7b3cbecd5babfe7d52f8aad
prangancode <senprangan@gmail.com>
2021-09-13 20:09:20 +0600

    card background set

--- js/app.js
+++ js/app.js
@@ -23,7 +23,27 @@ const showProducts = (products) => {
       <h2>Price: $ ${product.price}</h2>
       
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button>
+      <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails(${product.id})" id="details-btn" class="btn btn-danger">Details</button>
+
+      <div class="modal " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class=" modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title text-center fw-bold fst-italic" id="exampleModalLabel"></h4>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <img class="product-image" id="modalBodyImg" src=""></img>
+        <p class="modal-title fw-light font-monospace text-success" id="modalBodyDesc"></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Done</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
       <div class="d-flex justify-content-around mx-5 my-3 fw-normal text-primary">
       <h6>Total Ratings: <span class="text-danger">${product.rating.count}</span></h6>
       <h6>Rate: <span class="text-danger">${product.rating.rate}</span></h6>
@@ -33,6 +53,7 @@ const showProducts = (products) => {
     document.getElementById("all-products").appendChild(div);
   }
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -86,3 +107,20 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// Showing Product Details in Modal
+const showDetails = (id) => {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => displayProductDetails(data));
+}
+
+const displayProductDetails = (data) => {
+  console.log(data);
+  const modalTitle = document.getElementById('exampleModalLabel');
+  modalTitle.innerText = `${data.title}`;
+  const modalImg = document.getElementById('modalBodyImg');
+  modalImg.src = `${data.image}`;
+  const description = document.getElementById('modalBodyDesc');
+  description.innerText = `${data.description}`;
+}
